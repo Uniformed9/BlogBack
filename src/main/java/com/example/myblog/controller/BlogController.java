4a897c9e304c62f49387e13a4136d8fc767978f2
Uniformed9 <1498964461@qq.com>
2023-06-20 11:09:19 +0800

    版本

--- src/main/java/com/example/myblog/controller/BlogController.java
+++ src/main/java/com/example/myblog/controller/BlogController.java
@@ -1,6 +1,8 @@
 package com.example.myblog.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.myblog.common.R;
+import com.example.myblog.common.UserHolder;
 import com.example.myblog.entity.Blog;
 import com.example.myblog.service.BlogService;
 import com.example.myblog.service.UserService;
@@ -9,8 +11,10 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/blog/{id}")
+@RequestMapping(path = "/blog")
 @Api(tags = "单个博客接口")
 public class BlogController {
     @Autowired
@@ -18,12 +22,17 @@ public class BlogController {
     @Autowired
     UserService userService;
     @ApiOperation(value = "获取博客的内容")
-    @GetMapping()
+    @GetMapping("{id}")
     public R getBlog(@PathVariable int id) {
         if (!blogService.isBlogExist(id)) return R.error("Blog " + id + " is not exist!");
         return R.success(blogService.getBlogById(id));
     }
-
+    @ApiOperation(value = "获取全部博客的内容")
+    @GetMapping("/getAll")
+    public  R<List<Blog>> getBlogAll(){
+        List<Blog> list = blogService.list();
+        return R.success(list);
+    }
     @ApiOperation(value = "获取博客标签")
     @GetMapping(path = "/tags")
     public R getBlogTags(@PathVariable int id) {
